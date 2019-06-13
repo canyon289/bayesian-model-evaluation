02f6dfafe8e650cc03f5cfeedad5754b12ed6519
Colin <ColCarroll@users.noreply.github.com>
2019-06-12 22:50:17 -0400

    Chapter 3 edits (#59)

--- notebooks/utils/metropolis_hastings.py
+++ notebooks/utils/metropolis_hastings.py
@@ -15,20 +15,24 @@ N_mu_30_sd_1_data = stats.norm.rvs(loc=30, scale=1, size=1000).flatten()
 
 
 def mh_sampler(data, samples=4, mu_init=.5, proposal_width=.5, mu_prior_mu=0, mu_prior_sd=1., offset=20):
-    """Basic Metropolis Hasting Sampler"""
+    """Basic Metropolis Hasting Sampler. Optimized a little."""
     mu_current = mu_init
-    posterior = [mu_current]
-    for i in range(samples):
+    posterior = []
+    prior_logpdf = stats.norm(mu_prior_mu, mu_prior_sd).logpdf
+    likelihood_logpdf = stats.norm(offset, 1).logpdf  # transform the inputs by subtracting the mean
+    data = np.array(data) # for safety reasons
+
+    for _ in range(samples):
         # Suggest new position
-        mu_proposal = stats.norm(mu_current, proposal_width).rvs()
+        mu_proposal = np.random.normal(mu_current, proposal_width)
 
         # Compute likelihood by multiplying probabilities of each data point
-        likelihood_current = stats.norm(mu_current+offset, 1).logpdf(data).sum()
-        likelihood_proposal = stats.norm(mu_proposal+offset, 1).logpdf(data).sum()
+        likelihood_current = likelihood_logpdf(data - mu_current).sum()
+        likelihood_proposal = likelihood_logpdf(data - mu_proposal).sum()
 
         # Compute prior probability of current and proposed mu
-        prior_current = stats.norm(mu_prior_mu, mu_prior_sd).logpdf(mu_current)
-        prior_proposal = stats.norm(mu_prior_mu, mu_prior_sd).logpdf(mu_proposal)
+        prior_current = prior_logpdf(mu_current)
+        prior_proposal = prior_logpdf(mu_proposal)
 
         p_current = likelihood_current + prior_current
         p_proposal = likelihood_proposal + prior_proposal
@@ -40,7 +44,6 @@ def mh_sampler(data, samples=4, mu_init=.5, proposal_width=.5, mu_prior_mu=0, mu
         accept = np.random.rand() < p_accept
 
         if accept:
-
             # Update position
             mu_current = mu_proposal
 
